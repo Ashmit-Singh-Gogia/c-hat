1f7008925220c95f76dccca57e72bc4567015ebe
Ashmit Singh Gogia <ashmit9955@gmail.com>
2026-05-08 23:22:29 +0530

    updated user model

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -3,11 +3,13 @@ package models
 import "time"
 
 type User struct {
-	ID        uint      `gorm:"primaryKey;autoIncrement" json:"id"`
-	GoogleID  string    `gorm:"uniqueIndex;not null" json:"google_id"`
-	Username  string    `gorm:"not null" json:"username"`
-	Email     string    `gorm:"uniqueIndex;not null" json:"email"`
-	Avatar    string    `json:"avatar"`
-	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	ID         uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	Provider   string    `gorm:"uniqueIndex:idx_provider_id;not null" json:"provider"`
+	ProviderID string    `gorm:"uniqueIndex:idx_provider_id;not null" json:"provider_id"`
+	Username   string    `gorm:"not null" json:"username"`
+	Email      string    `gorm:"not null" json:"email"`
+	Avatar     string    `json:"avatar"`
+	Password   string    `json:"-"` // Leave empty for OAuth, use later for Email/Pass
+	CreatedAt  time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt  time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
